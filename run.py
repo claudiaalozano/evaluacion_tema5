be9d9276242ea1edafaf4e0e65f85452bd7ce757
claudiaalozano <clau.lozano.perez@gmail.com>
2022-12-21 13:44:13 +0100

    c

--- run.py
+++ run.py
@@ -1,16 +1,30 @@
-from calculos import *
-from reloj import *
-
 
 if "__name__" == "__main__":
-    
     ejercicio = int(input("Elija el ejercicio: "))
-    
     if ejercicio == 1:
-       print( "{} + {} = {}".format(a, b, suma(a, b) ) )
-       print( "{} - {} = {}".format(b, d, resta(b, d) ) )
-       print( "{} * {} = {}".format(b, b, multiplicacion(b, b) ) )
-       print( "{} / {} = {}".format(a, c, division(a, c) ) ) 
-
+        from calculos import *
+        print( "{} + {} = {}".format(a, b, suma(a, b) ) )
+        print( "{} - {} = {}".format(b, d, resta(b, d) ) )
+        print( "{} * {} = {}".format(b, b, multiplicacion(b, b) ) )
+        print( "{} / {} = {}".format(a, c, division(a, c) ) ) 
+    elif ejercicio == 2:
+        from contador import *
+        print(contador)
+        with open("contador.txt", "w") as f:
+            f.write(str(contador))
+    elif ejercicio == 3:
+        from gestor import *
+        gestor = Gestor()
+        caballero= Personaje("Caballero", 4,2, 4, 2)
+        gestor.agregar_personaje(caballero)
+        arquero= Personaje("Arquero", 2,4,1,8)
+        gestor.agregar_personaje(arquero)
+        guerrero= Personaje("Guerrero", 2,4,2,4)
+        gestor.agregar_personaje(guerrero)
+        gestor.mostrar_personaje()
+        gestor.borrar_personaje("Arquero")
+        gestor.mostrar_personaje()
+        gestor.guardar()
     elif ejercicio == 4:
-         print(hora_actual.strftime("%H:%M:%S"))
+        from reloj import *
+        print(hora_actual.strftime("%H:%M:%S"))
